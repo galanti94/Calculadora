d3bcd7d176cff30e12114bc873127f161f291797
Guilherme R Galanti <galanti_imp@hotmail.com>
2022-07-07 01:12:19 -0300

    Adicionando operadores

--- app/src/main/java/com/guilhermegalanti/calculadora/MainActivity.kt
+++ app/src/main/java/com/guilhermegalanti/calculadora/MainActivity.kt
@@ -6,8 +6,16 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.lang.ArithmeticException
+import java.lang.Math.round
 
 class MainActivity : AppCompatActivity() {
+
+    //TODO enxugar estas flags
+    var ultimoEraPonto: Boolean = false
+    var ultimoEraNumero: Boolean = true
+    var apertarCLR: Boolean = true
+    var inicio: Boolean = true
 
     private var tvTelaCalculadora: TextView? = null
 
@@ -19,6 +27,83 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun cliclarTecla(view: View) {
+        if (inicio) {
+            tvTelaCalculadora?.text = ""
+            inicio = false
+        }
+
         tvTelaCalculadora?.append((view as Button).text) // Somente o botão tem a propriedade .text
+        ultimoEraNumero = true
+        ultimoEraPonto = false
     }
+
+    fun teclaCLR(view: View) {
+        tvTelaCalculadora?.text = "0"
+        apertarCLR = true
+        inicio = true
+    }
+
+    fun pontoDecimal(view: View) {
+        if(ultimoEraNumero && !ultimoEraPonto && apertarCLR) {
+            tvTelaCalculadora?.append((view as Button).text)
+            ultimoEraNumero = false
+            ultimoEraPonto = true
+            apertarCLR = false
+            inicio = false
+        }
+    }
+
+    private fun operadorAdicionado(naTela: String): Boolean {
+        return naTela.contains("+")
+                || naTela.contains("-")
+                || naTela.contains("*")
+                || naTela.contains("/")
+    }
+
+    fun operacao(view: View){
+        if(!inicio) {
+            tvTelaCalculadora?.text.let {
+                if (ultimoEraNumero && !operadorAdicionado(it.toString())) {
+                    tvTelaCalculadora?.append((view as Button).text)
+                    ultimoEraNumero = false
+                    ultimoEraPonto = false
+                }
+            }
+        }
+
+    }
+
+    fun teclaIgual(view: View) {
+        if(ultimoEraNumero) {
+            var textoNaTela = tvTelaCalculadora?.text.toString() // text é uma sequência de caracteres
+            if(operadorAdicionado(textoNaTela)){
+                try {
+                    if(textoNaTela.contains("-")){
+                        val splitText = textoNaTela.split("-")
+                        val resultado = splitText[0].toInt() - splitText[1].toInt()
+                        tvTelaCalculadora?.text = resultado.toString()
+                        inicio = true
+                    } else if(textoNaTela.contains("+")){
+                        val splitText = textoNaTela.split("+")
+                        val resultado = splitText[0].toInt() + splitText[1].toInt()
+                        tvTelaCalculadora?.text = resultado.toString()
+                        inicio = true
+                    } else if(textoNaTela.contains("/")){
+                        val splitText = textoNaTela.split("/")
+                        val resultado = splitText[0].toDouble() / splitText[1].toDouble()
+                        tvTelaCalculadora?.text = String.format("%.2f", resultado)
+                        inicio = true
+                    } else if(textoNaTela.contains("*")){
+                        val splitText = textoNaTela.split("*")
+                        val resultado = splitText[0].toInt() * splitText[1].toInt()
+                        tvTelaCalculadora?.text = resultado.toString()
+                        inicio = true
+                    }
+                } catch (e: ArithmeticException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
 }
